0440340ab277e4fbdf718eb8d3fcbff3cb47c9ea
Alexandræ <114509275+Usernamealexandraeisnotavailable@users.noreply.github.com>
2025-07-15 23:17:39 +0200

    Add files via upload

---
+++ experiences/valeurs_p@0.py
@@ -0,0 +1,10 @@
+import csv
+import modules.inferences
+
+prevalences_inferences = modules.inferences.calculer_prevalences_inferences()
+
+"""
+    modules.inferences.calculer_ecarts_types_prevalences_entre_modeles (
+        prevalences_inferences
+    )
+"""
